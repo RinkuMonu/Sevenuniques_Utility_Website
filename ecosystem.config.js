560523bac647149dff7e62fd4208944c8a1c1a03
RinkuMonu <rinkuyadav9460@gmail.com>
2025-10-09 10:36:33 +0530

    changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "nextjs-static-site",
+      name: "sevenuniques.com",
       script: "serve",
       args: ["-s", "out", "-l", "3000"],  // out folder ko port 3000 par serve karega
       cwd: "/home/sevenu/public_html/Sevenuniques_Utility_Website", // project ka path
