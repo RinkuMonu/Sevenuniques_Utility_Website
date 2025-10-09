fc0d3db0e3fb1c85f5adc9e758aabe0551fc5257
RinkuMonu <rinkuyadav9460@gmail.com>
2025-10-09 10:41:27 +0530

    changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,12 @@ module.exports = {
   apps: [
     {
       name: "sevenuniques.com",
-      script: "serve",
-      args: ["-s", "out", "-l", "3000"],  // out folder ko port 3000 par serve karega
-      cwd: "/home/sevenu/public_html/Sevenuniques_Utility_Website", // project ka path
+      script: "npm",
+      args: "start", // next start
+      cwd: "/home/sevenu/public_html/Sevenuniques_Utility_Website",
       env: {
         NODE_ENV: "production",
+        PORT: 3000,
       },
     },
   ],
