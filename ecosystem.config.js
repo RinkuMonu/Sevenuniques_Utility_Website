13ede3c9af797899c5394acadcc83fc7de803801
RinkuMonu <rinkuyadav9460@gmail.com>
2025-10-09 10:33:48 +0530

    sdfghj

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "nextjs-static-site",
       script: "serve",
-      args: "-s out -l 3000",  // out folder ko port 3000 par serve karega
+      args: ["-s", "out", "-l", "3000"],  // out folder ko port 3000 par serve karega
       cwd: "/home/sevenu/public_html/Sevenuniques_Utility_Website", // project ka path
       env: {
         NODE_ENV: "production",
