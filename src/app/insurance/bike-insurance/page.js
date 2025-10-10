21d4cd09ae9ace71fb1607884d6f6354e4fa808b
RinkuMonu <rinkuyadav9460@gmail.com>
2025-10-10 15:34:07 +0530

    sdfghjkl

--- src/app/insurance/bike-insurance/page.js
+++ src/app/insurance/bike-insurance/page.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import Herobanner from '../../../../components/Bike Insurance/Herobanner'
 import Features from '../../../../components/Bike Insurance/Features'
-import {WhyChooseUs}  from '../../../../components/Bike Insurance/WhyChooseUs'
+import WhyChooseUs  from '../../../../components/Bike Insurance/WhyChooseUs'
 import  FAQ  from '../../../../components/Bike Insurance/FAQ'
-import  {Process}  from '../../../../components/Bike Insurance/Process'
+import  Process  from '../../../../components/Bike Insurance/Process'
 import ContactBanner from '../../../../components/ContactBanner'
 import AboutSection from '../../../../components/Bike Insurance/AboutSection'
 import { Shield, Clock, DollarSign, FileText, Headphones, Award } from "lucide-react"
