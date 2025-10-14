4445a2db3491b9bee16144ac26f0c9248e63d031
DevikaChhipa <chhipadevika0@gmail.com>
2025-10-14 10:30:12 +0530

    changes

--- components/PancardAllotment2.js
+++ components/PancardAllotment2.js
@@ -5,19 +5,15 @@ import Image from "next/image";
 export default function PancardAllotment2() {
   return (
     <section className="bg-[#e8f0f7] rounded-xl flex flex-col md:flex-row justify-evenly items-start shadow-sm">
-
   <div className="">
-        <Image src="/image/pancard-foreign3.png" width={430} height={430} alt="image" />
+        <Image src="/image/pancard-foreign3.png" width={360} height={360} alt="image" />
       </div>
-
-
          {/* Left Side */}
       <div className="max-w-xl p-12">
         <h2 className="text-4xl font-bold text-gray-900 leading-snug">
           Application for Allotment of New PAN (Form 49AA) <br />
           <span className="text-blue-900">for Foreign Citizens</span>
         </h2>
-
         <p className="text-gray-700 mt-6 leading-relaxed text-base relative">
           This type is assigned to foreign individuals and entities that did not
           receive a PAN previously.
@@ -32,7 +28,6 @@ export default function PancardAllotment2() {
           our online PAN services.
           <span className="absolute left-0 bottom-[-25px] w-full h-[2px] bg-[#FFD84C] rounded-full"></span>
         </p>
-
         <div className="md:mt-18 w-full">
   <div className="flex flex-wrap justify-center gap-4">
     {[
@@ -40,7 +35,7 @@ export default function PancardAllotment2() {
       "Read Guidelines",
       "Read Instructions",
       "Documents Required",
-      "Do’s & Don’ts",
+      "Do’s & Dont’s",
       "Designated Centers",
     ].map((item, i) => (
       <a
@@ -54,7 +49,6 @@ export default function PancardAllotment2() {
     ))}
   </div>
 </div>
-
       </div>
     </section>
   );
