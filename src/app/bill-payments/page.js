912854a1db6fe694592dca976d9f057911a8e3f3
anisha-max <parmaranisha761@gmail.com>
2025-10-30 10:23:02 +0530

    bbps page

--- src/app/bill-payments/page.js
+++ src/app/bill-payments/page.js
@@ -19,14 +19,9 @@ function page() {
   };
   return (
     <>
-      <div className="relative flex flex-col md:flex-row items-center justify-between ">
-        <div
-          className="absolute inset-0 bg-cover bg-center hidden md:block"
-          style={{ backgroundImage: `url(${data?.bgImage})` }}
-        ></div>
-        <div className=" z-10 mx-auto">
-          <div className=" space-y-6 md:space-y-0 items-center">
-            <div className=" px-10 py-10 lg:py-16 rounded-2xl overflow-hidden max-w-2xl w-full">
+      <div className=" bg-no-repeat bg-cover bg-center"   style={{ backgroundImage: `url(${data?.bgImage})` , }}>
+          <div className="max-w-7xl mx-auto space-y-6 md:space-y-0 px-4 lg:px-0">
+            <div className=" py-10 lg:py-16 rounded-2xl w-full">
               <div className=" z-10">
                 <p className="text-sm font-bold uppercase tracking-widest text-[#0C3D4C]">
                   {data?.subheading}
@@ -48,22 +43,9 @@ function page() {
               </div>
             </div>
 
-            {/* <div className="w-full  flex justify-center items-end">
-              {data.centerImage ? (
-                <Image
-                  src={data.centerImage}
-                  height={700}
-                  width={600}
-                  alt="center image"
-                  className="md:pt-4 lg:pt-16  lg:h-[86vh] lg:w-[80vh] z-20"
-                />
-              ) : (
-                ""
-              )}
-            </div> */}
           </div>
         </div>
-      </div>
+     
       <div className="bg-[#EEF2F5] relative py-10 overflow-visible">
         <div className="max-w-7xl mx-auto px-4 lg:px-0 relative">
           <div className="grid grid-cols-1 md:grid-cols-2 ">
@@ -75,8 +57,8 @@ function page() {
                 height={400}
               />
             </div>
-            <div className="md:block relative md:h-[200px] mt-10 md:mt-0 ">
-                            <div className="absolute -top-96 left-0 rounded-xl w-[350px] lg:w-[450px] h-[604px] z-20 hidden md:block ">
+            <div className="lg:block relative md:h-[200px] mt-10 md:mt-0 ">
+                            <div className="absolute -top-96 left-0 rounded-xl w-[350px] md:w-[400px] lg:w-[600px] h-[604px] z-20 hidden md:block ">
                                 <Image
                                     src="/image/service-img.png"
                                     alt="center image"
