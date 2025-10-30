04f176f8fccb54bb707d536868fff5623d4d0ac5
anisha-max <parmaranisha761@gmail.com>
2025-10-30 11:01:40 +0530

    small changes in aeps page2

--- components/AepsAdvantages.js
+++ components/AepsAdvantages.js
@@ -60,7 +60,7 @@ const AepsAdvantages = () => {
           across India.
         </p>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 lg:px-7">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
   {advantages.map((item) => (
     <div
       key={item.id}
