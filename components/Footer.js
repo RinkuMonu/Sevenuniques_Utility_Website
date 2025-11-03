59ef61dc7a3d5b3ce45d17c3aac8ea72fd62d88d
RinkuMonu <rinkuyadav9460@gmail.com>
2025-11-03 11:15:09 +0530

    fix: correct typo in footer text

--- components/Footer.js
+++ components/Footer.js
@@ -153,7 +153,7 @@ const Footer = () => {
             </div>
             <div className="md:col-span-2">
               <p className=" text-white text-sm font-light">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an
               </p>
             </div>
 
