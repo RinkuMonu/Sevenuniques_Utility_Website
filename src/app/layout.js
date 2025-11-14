410a267230c6fbd6055b19c0b39e21e06575e5c7
RinkuMonu <rinkuyadav9460@gmail.com>
2025-11-14 14:53:21 +0530

    fix: update favicon path to new logo location

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
        <head>
-        <link rel="icon" href="/image/favicon.ico" />
+        <link rel="icon" href="/white/sevenunique-logo.ico" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
