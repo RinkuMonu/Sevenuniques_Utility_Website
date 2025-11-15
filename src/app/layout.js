0d99741442fe55a953cf36c2687bc225f637df12
RinkuMonu <rinkuyadav9460@gmail.com>
2025-11-15 12:35:59 +0530

    fix: format layout.js for improved readability and consistency

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,25 +16,50 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Finunique Bill Payment, Recharge, Insurance, Booking, Credit Card, pancard, Loan",
-  description: "Finunique Bill Payment, Recharge, Insurance, Booking, Credit Card, pancard, Loan",
+  title:
+    "Finunique Bill Payment, Recharge, Insurance, Booking, Credit Card, pancard, Loan",
+  description:
+    "Finunique Bill Payment, Recharge, Insurance, Booking, Credit Card, pancard, Loan",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-       <head>
+      <head>
         <link rel="icon" href="/white/sevenunique-logo.ico" />
+
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5TKFRRHQ');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5TKFRRHQ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
 
-  <Header />
+        <Header />
         {children}
         <HomeQRBanner />
         <PreFooterSection />
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
