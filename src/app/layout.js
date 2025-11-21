5fb7758ef72ac4b3c855fb92de59fbbbe68e4878
RinkuMonu <rinkuyadav9460@gmail.com>
2025-11-21 15:16:49 +0530

    asdfghjk

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import PreFooterSection from "../../components/Prefooter";
 import HomeQRBanner from "../../components/HomeQRSection";
-import SEO from "../../components/SEO"; // ⭐ Yaha import karo
+import SEO from "../../components/SEO/SEO"; // ⭐ Yaha import karo
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
