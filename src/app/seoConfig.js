d5392c900bfba7ea3cdc49f193cb1ab50d8768a2
RinkuMonu <rinkuyadav9460@gmail.com>
2025-11-21 14:51:59 +0530

    bbbbbbb

---
+++ src/app/seoConfig.js
@@ -0,0 +1,25 @@
+// seoConfig.js
+'use client'
+
+import axios from "axios";
+
+export default async function seoConfig(path) {
+  try {
+    const token = "jibhfiugh84t3324fefei#*fef"; // apna token yaha rakho
+
+    const SEO = await axios.get(
+      `https://cms.sevenunique.com/apis/SEO/get-seo-content.php?website_id=6&page_slug=https://www.sevenunique.com${path}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`, // agar bearer token use hota hai
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    return SEO.data.data[0];
+  } catch (error) {
+    console.log("SEO error", error);
+    return null;
+  }
+}
