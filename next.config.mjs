5de983920299bd01f2069384858f8ed9d360fa2b
Nandiniii28 <singhnandini9358@gmail.com>
2026-01-28 10:40:55 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-};
+  images: {
+    unoptimized: true
+  }
+}
 
-export default nextConfig;
-
+export default nextConfig
